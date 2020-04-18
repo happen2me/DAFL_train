4d2aef5cdf5a3fb6dece86f992c9d00f02258c61
Yuanchun <immr.shen@gmail.com>
2020-04-18 18:30:17 +0800

    completed generator_train.py

--- DAFL/generator_train.py
+++ DAFL/generator_train.py
@@ -1,9 +1,3 @@
-#Copyright (C) 2019. Huawei Technologies Co., Ltd. All rights reserved.
-
-#This program is free software; you can redistribute it and/or modify it under the terms of the BSD 3-Clause License.
-
-#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
-
 import argparse
 import os
 import numpy as np
@@ -49,8 +43,6 @@ img_shape = (opt.channels, opt.img_size, opt.img_size)
 cuda = True
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-accr = 0
-accr_best = 0
 
 
 class Generator(nn.Module):
@@ -98,82 +90,18 @@ def run():
     teacher = nn.DataParallel(teacher)
     generator = nn.DataParallel(generator)
 
-    def kdloss(y, teacher_scores):
-        p = F.log_softmax(y, dim=1)
-        q = F.softmax(teacher_scores, dim=1)
-        l_kl = F.kl_div(p, q, size_average=False)  / y.shape[0]
-        return l_kl
-
-    if opt.dataset == 'MNIST':
-        # Configure data loader
-        net = LeNet5Half().to(device)
-        net = nn.DataParallel(net)
-        data_test = MNIST(opt.data,
-                          train=False,
-                          transform=transforms.Compose([
-                              transforms.Resize((32, 32)),
-                              transforms.ToTensor(),
-                              transforms.Normalize((0.1307,), (0.3081,))
-                              ]))
-        data_test_loader = DataLoader(data_test, batch_size=64, num_workers=1, shuffle=False)
-
-        # Optimizers
-        optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr_G)
-        optimizer_S = torch.optim.Adam(net.parameters(), lr=opt.lr_S)
-
-    if opt.dataset != 'MNIST':
-        transform_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-        if opt.dataset == 'cifar10':
-            net = resnet.ResNet18().to(device)
-            net = nn.DataParallel(net)
-            data_test = CIFAR10(opt.data,
-                              train=False,
-                              transform=transform_test)
-        if opt.dataset == 'cifar100':
-            net = resnet.ResNet18(num_classes=100).to(device)
-            net = nn.DataParallel(net)
-            data_test = CIFAR100(opt.data,
-                              train=False,
-                              transform=transform_test)
-        data_test_loader = DataLoader(data_test, batch_size=opt.batch_size, num_workers=0)
-
-        # Optimizers
-        optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr_G)
-
-        optimizer_S = torch.optim.SGD(net.parameters(), lr=opt.lr_S, momentum=0.9, weight_decay=5e-4)
-
-
-    def adjust_learning_rate(optimizer, epoch, learing_rate):
-        if epoch < 800:
-            lr = learing_rate
-        elif epoch < 1600:
-            lr = 0.1*learing_rate
-        else:
-            lr = 0.01*learing_rate
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-
+    # Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr_G)
 
     # ----------
     #  Training
     # ----------
-
-    batches_done = 0
     for epoch in range(opt.n_epochs):
 
-        total_correct = 0
-        avg_loss = 0.0
-        if opt.dataset != 'MNIST':
-            adjust_learning_rate(optimizer_S, epoch, opt.lr_S)
-
         for i in range(120):
-            net.train()
+            generator.train()
             z = Variable(torch.randn(opt.batch_size, opt.latent_dim)).to(device)
             optimizer_G.zero_grad()
-            optimizer_S.zero_grad()
             gen_imgs = generator(z)
             outputs_T, features_T = teacher(gen_imgs, out_feature=True)
             pred = outputs_T.data.max(1)[1]
@@ -182,31 +110,13 @@ def run():
             softmax_o_T = torch.nn.functional.softmax(outputs_T, dim = 1).mean(dim = 0)
             loss_information_entropy = (softmax_o_T * torch.log(softmax_o_T)).sum()
             loss = loss_one_hot * opt.oh + loss_information_entropy * opt.ie + loss_activation * opt.a
-            loss_kd = kdloss(net(gen_imgs.detach()), outputs_T.detach())
-            loss += loss_kd
             loss.backward()
             optimizer_G.step()
-            optimizer_S.step()
             if i == 1:
-                print ("[Epoch %d/%d] [loss_oh: %f] [loss_ie: %f] [loss_a: %f] [loss_kd: %f]" % (epoch, opt.n_epochs,loss_one_hot.item(), loss_information_entropy.item(), loss_activation.item(), loss_kd.item()))
+                print ("[Epoch %d/%d] [loss_oh: %f] [loss_ie: %f] [loss_a: %f]" % (epoch, opt.n_epochs,loss_one_hot.item(), loss_information_entropy.item(), loss_activation.item()))
 
-        with torch.no_grad():
-            for i, (images, labels) in enumerate(data_test_loader):
-                images = images.to(device)
-                labels = labels.to(device)
-                net.eval()
-                output = net(images)
-                avg_loss += criterion(output, labels).sum()
-                pred = output.data.max(1)[1]
-                total_correct += pred.eq(labels.data.view_as(pred)).sum()
-
-        avg_loss /= len(data_test)
-        print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.data.item(), float(total_correct) / len(data_test)))
-        accr = round(float(total_correct) / len(data_test), 4)
-        if accr > accr_best:
-            torch.save(net,opt.output_dir + 'student')
-            torch.save(generator.state_dict(), opt.output_dir + "generator.pt")
-            accr_best = accr
+        torch.save(generator.state_dict(), opt.output_dir + "generator_only.pt")
+        print("generator saved at ", opt.output_dir + "generator_only.pt")
 
 
 if __name__ == "__main__":
