e956bb5b10f2d9308e6a7363dc4b750a5da8b835
Yuanchun <immr.shen@gmail.com>
2020-04-18 13:16:41 +0800

    complete test generator

---
+++ DAFL/test_generator.py
@@ -0,0 +1,34 @@
+import torch
+import matplotlib.pyplot as plt
+from DAFL_train import Generator
+
+generator_path = "/cache/models/generator.pt"
+latent_dim = 100
+
+
+def partial_load(model_cls, model_path, device):
+    model = model_cls().to(device)
+    model.eval()
+    saved_state_dict = torch.load(model_path, map_location=device).module.state_dict()
+    model_state_dict = model.state_dict()
+    # filter state dict
+    filtered_dict = {k: v for k, v in saved_state_dict.items() if k in model_state_dict}
+    model_state_dict.update(filtered_dict)
+    model.load_state_dict(model_state_dict)
+    return model
+
+
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    generator = partial_load(Generator, generator_path, device)
+
+    print("generator params:")
+    for param in generator.parameters():
+        print(param)
+
+    for i in range(5):
+        test_rand = torch.randn(latent_dim)
+        output = generator(test_rand)
+        print(output[0])
+        plt.imshow(output[0].detach().numpy())
+        plt.show()
