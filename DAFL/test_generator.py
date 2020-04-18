2d8afca247102e9ac460788cf0b42cfd79802282
Yuanchun <immr.shen@gmail.com>
2020-04-18 14:55:23 +0800

    correct test_generator.py

--- DAFL/test_generator.py
+++ DAFL/test_generator.py
@@ -2,14 +2,14 @@ import torch
 import matplotlib.pyplot as plt
 from DAFL_train import Generator
 
-generator_path = "/cache/models/generator.pt"
+generator_path = "cache/models/generator.pt"
 latent_dim = 100
 
 
 def partial_load(model_cls, model_path, device):
     model = model_cls().to(device)
     model.eval()
-    saved_state_dict = torch.load(model_path, map_location=device).module.state_dict()
+    saved_state_dict = torch.load(model_path, map_location=device)
     model_state_dict = model.state_dict()
     # filter state dict
     filtered_dict = {k: v for k, v in saved_state_dict.items() if k in model_state_dict}
@@ -27,8 +27,9 @@ if __name__ == "__main__":
         print(param)
 
     for i in range(5):
-        test_rand = torch.randn(latent_dim)
+        test_rand = torch.randn(64, latent_dim)  # 64 is batch size
         output = generator(test_rand)
-        print(output[0])
-        plt.imshow(output[0].detach().numpy())
+        print("output[0] shape is ", output[0].shape)
+
+        plt.imshow(output[0][0].detach().numpy())
         plt.show()
